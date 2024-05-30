3969d936217476de39d5702901b6272c006e5bc9
amandinedestexhe <e37923@promotion-sociale.be>
2024-05-30 15:55:40 +0200

    route par défaut pages.home

--- app/views/templates/partials/_main.php
+++ app/views/templates/partials/_main.php
@@ -3,9 +3,6 @@
       <!-- Aside -->
       <?php include_once '../app/views/templates/partials/_aside.php'?>
       <main class="w-full md:w-3/4 p-3">
-      <h2 class="text-2xl font-bold mb-4">Popular books</h2>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <?php echo $content;?>
-        </div>
+      <?php echo $content;?>
       </main>
 </div>
